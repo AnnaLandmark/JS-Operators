fca823f773e0fba2b359b8ae95aed9731c5b2a70
Markaija <christina.landmark@hotmail.com>
2022-02-15 17:32:18 +0100

    Added operators js file

---
+++ Operators.js
@@ -0,0 +1,94 @@
+'use strict';
+
+//assignemnt 1
+/*
+let numOfDaysUntilWeekend = 2;
+numOfDaysUntilWeekend -= 1;
+
+1. Use console.log() to log the value just after the variable has been declared
+2.  Use console.log() to log the value right after the value of the variable has been changed. 
+3. Tell what it does 
+*/
+//x = 2
+let numOfDaysUntilWeekend = 2;
+console.log(numOfDaysUntilWeekend);//2
+numOfDaysUntillWeekend -= 1;
+console.log(numOfDaysUntilWeekend);//1
+
+//assignemnt 2
+//Addition to numbers
+//x = x += 5
+let x = 2;
+let y = 5;
+let z = x += y;
+console.log(x + y);
+
+//assignemnt 3
+//Subtraction to numbers
+//x = x -= 1
+let x = 2;
+let y = 5;
+let z = x -= y;
+console.log(x);
+
+
+//assignemnt 4
+//write two sentences in two variables and connect them together 
+let x = "Hello";
+let y = "World";
+console.log(x + y)
+
+//assignemnt 5
+//write two variables, one number and one string. add them together
+let x = "Hello";
+let y = 5;
+console.log(x + y)
+
+//assignemnt 6
+//write the answers in a comment
+let r = 15;
+let s = 5;
+let t = r + s; //20
+let u = r - s; //10
+let v = t / u; //2
+let x = r * v; //30
+let y = s % v; //0
+
+//assignemnt 7
+//Why does it not write out? 
+let r = 61; 
+let c = 1;
+let x = r - 32 + c; //30
+let y = r++ * 4; //2
+if(x == 30 || y == 2 ){
+ console.log("yeah you got it");
+}
+//change a number in a variable, so it prints out the text
+
+
+
+//assignemnt 8
+//make two vaiables. One variable must contain a number and the other one must contain a prompt, 
+//where the user can enter a number. Match the two numbers and print out the answear
+//TIP: search prompt
+let vari_3 = prompt("Please enter your name", "Harry Potter");
+let vari_4 = 5;
+console.log(vari_3 + vari_4);
+
+
+//assignment 9
+//write these numbers in the console and write the output. Explain you thing this happens
+    0.1 + 0.2;
+    0.1 + 0.2 === 0.3;
+    3  - 1
+    3  + 1
+    '3' - 1 
+    '3' + 1 
+    '222' - -'111'
+
+no
+
+//assignemnt 9.1
+//Write a JavaScript program to determine whether a given year is a leap year 
+
+
